6ed779409ecbbbacb634ad06a25fac8abdd99622
ramity <lcb005@uark.edu>
2016-05-31 12:02:11 -0500

    init commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+	<head>
+		<title>Health tracker</title>
+		<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+		<link href="https://ramity.com/projects/health/css/index.css" rel="stylesheet" type="text/css" >
+	</head>
+	<body>
+		<div id="topbar">
+			<div id="topbarinr">
+				<div id="topbarlogo">Ramity</div>
+				<div id="topbarlogin">
+					<a href="https://ramity.com/projects/health/login">login</a>
+					 | 
+					<a href="https://ramity.com/projects/health/login">register</a>
+				</div>
+			</div>
+		</div>
+		<div id="content">
+			<div id="contentinr">
+				<div id="contentprogress">
+					<div class="contenttitle">Daily summary</div>
+					<div id="contentcalorietop">
+						<div id="contentcalorieremaining">
+							<div id="contentcalorieremainingvalue">1234 calories remaining</div>
+							<div id="contentcalorieremainingbar">
+								<div id="contentcalorieremainingbarvalue" style="width:400px"></div>
+							</div>
+						</div>
+						<a id="contentcalorielogbutton">Add to log</a>
+					</div>
+					<div id="contentcaloriebottom">
+						<div class="contentcalorieitem">
+							<div class="contentcaloriename">Goal</div>
+							<div class="contentcalorievalue">1800</div>
+						</div>
+						<div class="contentcalorieitem">
+							<div class="contentcaloriename">Food</div>
+							<div class="contentcalorievalue">1245</div>
+						</div>
+						<div class="contentcalorieitem">
+							<div class="contentcaloriename">Exercise</div>
+							<div class="contentcalorievalue">-95</div>
+						</div>
+						<div class="contentcalorieitem">
+							<div class="contentcaloriename">Net</div>
+							<div class="contentcalorievalue">93</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
